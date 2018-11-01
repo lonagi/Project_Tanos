3b38a63d540c97e86e8e6524ab45ba146101b601
Sasha <40319171+lonagi@users.noreply.github.com>
2018-11-02 01:46:38 +0200

    Start python file
    -Создал главный рабочий файл
    -Внёс главные строчки
    -После этого загрузил по адресу http://dev.lonagi.pw/python

---
+++ index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+print "Content-type: text/html\n"
+print "\n\n"
+print "<html>"
+print "<head>"
+print "<title>Test</title>"
+print "</head>"
+print "<body>"
+print (
+"<div>Hello world!"
+"</div>"
+)
+print "</body>"
+print "</html>"
