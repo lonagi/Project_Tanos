9503d5fc6281515df839b9c0fd98174a69007faf
Sasha <40319171+lonagi@users.noreply.github.com>
2018-11-02 13:23:08 +0200

    Connect framework
    -Добавил Bootstrap и JQuery
    -Установил размер страницы на 100%

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
-#file version 3
-print "Content-type: text/html\n" #it is html
+#file version 4
+print "Content-type: text/html; charset=utf-8\n" #it is html
 print "\n\n" #breaking without html
 print "<html>" 
-print "<head>" 
+print "<head>"
+print '<link href="http://ex.nvg-team.com/bootstrap2.css" rel="stylesheet" type="text/css" media="all"/>\n' #Connect bootstrap css
+print '<meta name="viewport" content="width=device-width, initial-scale=1">\n' #set size display 100%
 print "<title>Test</title>" #title , ok, you know. html
 print "</head>"
 print "<body>"
@@ -13,6 +15,12 @@ print (
   "<br>" #html breaking
   "sos" #your text
 "</div>"
+)
+print (
+"<footer>"
+"<script src='http://ex.nvg-team.com/jquery-3.3.1.js'></script>" #connect jquery
+"<script src='http://ex.nvg-team.com/bootstrap2.js'></script>" #connect bootstrap js framework
+"</footer>"
 )
 print "</body>"
 print "</html>"
