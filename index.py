11f00519123d54ab0b92627603ae85c964079b3a
Sasha <40319171+lonagi@users.noreply.github.com>
2018-11-02 12:40:50 +0200

    Корректировка + комменты
    Написал свои комментарии + поменял твой принт

--- index.py
+++ index.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
-#file version 2
-print "Content-type: text/html\n"
-print "\n\n"
-print "<html>"
-print "<head>"
-print "<title>Test</title>"
+#file version 3
+print "Content-type: text/html\n" #Показываем, что это html
+print "\n\n" #на новую строчку
+print "<html>" 
+print "<head>" 
+print "<title>Test</title>" #заголовок страницы
 print "</head>"
 print "<body>"
 print (
-"<div>Hello world!"
+"<div>"
+  "Hello world!" #мой текст
+  "<br>" #отступ на новую строчку в html'е
+  "sos" #твой теперь тут
 "</div>"
 )
 print "</body>"
 print "</html>"
-print("sos")
