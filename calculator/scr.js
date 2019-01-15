75c855beb1fc533cb41583d12286e6078218af7c
Junumboxo <44647094+Junumboxo@users.noreply.github.com>
2019-01-15 20:59:04 +0200

    Update JS 2
    -object
    +class

--- calculator/scr.js
+++ calculator/scr.js
@@ -1,23 +1,24 @@
-var operationUser = new Operation();
-function Operation() {
-	this.val1Hldr = document.getElementById("value1");
-	this.val2Hldr = document.getElementById("value2");
-	this.rezHldr = document.getElementById("rez");
+class Calculate {
+	constructor() {
+		this.val1Hldr = document.getElementById("value1");
+		this.val2Hldr = document.getElementById("value2");
+		this.rezHldr = document.getElementById("rez");
 
-	this.value1 = 0;
-	this.value2 = 0;
-	this.rez = 0;
+		this.value1 = 0;
+		this.value2 = 0;
+		this.rez = 0;
+	}
 
-	this.initVars = function() {
+	initVars() {
 		this.value1 = parseFloat(this.val1Hldr.value);
 		this.value2 = parseFloat(this.val2Hldr.value);
-	};
+	}
 
-	this.displayRez = function() {
+	displayRez() {
 		this.rezHldr.value = this.rez;
-	};
+	}
 
-	this.validateNmbr = function() {
+	validateNmbr() {
 		var pattern = new RegExp("^[0-9]{0,5}[\.]?[0-9]{0,5}$");
 		if(!(pattern.test(this.val1Hldr.value))) {
 			this.val1Hldr.value = this.val1Hldr.value.substr(0, this.val1Hldr.value.length - 1);
@@ -25,35 +26,37 @@ function Operation() {
 		if(!(pattern.test(this.val2Hldr.value))) {
 			this.val2Hldr.value = this.val2Hldr.value.substr(0, this.val2Hldr.value.length - 1);
 		}
-	};
+	}
 
-	this.getSum = function() {
+	getSum() {
 		this.initVars();
 		this.rez = ((this.val1Hldr.value == "") || (this.val2Hldr.value == "")) ? "insert number" : this.value1 + this.value2;
 		this.displayRez();
-	};
+	}
 
-	this.getDiff = function() {
+	getDiff() {
 		this.initVars();
 		this.rez = ((this.val1Hldr.value == "") || (this.val2Hldr.value == "")) ? "insert number" : this.value1 - this.value2;
 		this.displayRez();
-	};
+	}
 
-	this.getProd = function() {
+	getProd() {
 		this.initVars();
 		this.rez = ((this.val1Hldr.value == "") || (this.val2Hldr.value == "")) ? "insert number" : this.value1 * this.value2;
 		this.displayRez();
-	};
+	}
 
-	this.getDiv = function() {
+	getDiv() {
 		this.initVars();
 		this.rez = ((this.val1Hldr.value == "") || (this.val2Hldr.value == "")) ? "insert number" : this.value1 / this.value2;
 		this.displayRez();
-	};
+	}
 
-	this.delValues = function() {
+	delValues() {
 		this.val1Hldr.value = "";
 		this.val2Hldr.value = "";
 		this.rezHldr.value = "";
-	};
-}
+	}
+};
+
+let operationUser = new Calculate();
