bf70e11dff013db90a6a99db95b7c8656f766bfb
Junumboxo <44647094+Junumboxo@users.noreply.github.com>
2019-06-07 21:54:55 +0300

    sorry im a loh

--- vot_etu/index.php
+++ vot_etu/index.php
@@ -52,7 +52,7 @@
   $listeningParasha = false;
   $year = 2019;
   $name = "Tanya";
-  echo $readingParasha."".$listeningParasha."".$year."".$name;
+  echo $readingParasha." ".$listeningParasha." ".$year." ".$name;
   ?>
   <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
   <hr class="my-4">
