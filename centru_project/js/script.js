0ea8f70a15b3056f65eaf520d2cd72fb889273ea
Junumboxo <44647094+Junumboxo@users.noreply.github.com>
2019-10-17 16:21:11 +0300

    Programes slider

--- centru_project/js/script.js
+++ centru_project/js/script.js
@@ -3,4 +3,24 @@ $(document).ready(function() {
 		setTimeout(function(){$(".blank").animate({ width: '800px'}, 1000)}, (2000));
 		setTimeout(function(){$(".logo").fadeIn(1000)}, (3000));
 	}
+	$('.owl-carousel').owlCarousel({
+	dots: true,
+	dotsEach: true,
+	responsiveClass:true,
+	responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        500:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+	});
 });
